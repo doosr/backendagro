e6a4fe2bd812aa18008b7a683183e6bb9f2ef13c
Dawser_belgacem <dawser.belgacem@mahdia.r-iset.tn>
2025-11-15 15:56:56 +0100

    aicha

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Middleware CORS - DOIT ÊTRE AVANT LES ROUTES
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
