b87f1b1f4d1a51a98168e51b9994a6be58b77037
Dawser_belgacem <dawser.belgacem@mahdia.r-iset.tn>
2025-11-30 20:02:12 +0100

    fkjdkdjk

--- debug-keys-2.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const SibApiV3Sdk = require('@sendinblue/client');
-
-console.log('Has ApiClient:', 'ApiClient' in SibApiV3Sdk);
-console.log('Has TransactionalEmailsApi:', 'TransactionalEmailsApi' in SibApiV3Sdk);
-console.log('Has SendSmtpEmail:', 'SendSmtpEmail' in SibApiV3Sdk);
--- debug-keys.js
+++
@@ -1,4 +0,0 @@
-require('dotenv').config();
-const SibApiV3Sdk = require('@sendinblue/client');
-
-console.log('KEYS:', Object.keys(SibApiV3Sdk));
